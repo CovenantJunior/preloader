cac10f3f441e26258efd18a09e71cbc52d59408d
Covenant Junior <covenant@gmail.com>
2022-10-24 22:06:28 +0100

    Best Practice - hide() replaced with fadeOut()

--- js/load.js
+++ js/load.js
@@ -1,12 +1,20 @@
 $(document).ready(() => {
-    //hide() method is to add a css property "display: none".
-    //Arguments in between hide(600) signify animation duration. It can be altered
 
-    //Instantly hides preloader after dcument has been loaded
-    //$('.preloader').hide(600);
+    //The fadeOut() method gradually changes the opacity, for selected elements, from visible to hidden (fading effect).
 
-    //Hides preloader after some seconds of load time (1000 = 1 second)
+    //Argument in between fadeOut(600) signify animation duration. It can be altered.
+
+    //Note: Hidden elements will not be displayed at all (no longer affects the layout of the page).
+
+    //Tip:
+        //This method is often used together with the fadeIn() method.
+        //And both can have a callback function after the animation duration
+
+    //Instantly fades out preloader after document has been loaded
+    //$('.preloader').fadeOut(600);
+
+    //Fades out preloader after some seconds of load time (1000 = 1 second)
     setTimeout(() => {
-        $('.preloader').hide(600);
+        $('.preloader').fadeOut(1000);
     }, 5000);
 })
